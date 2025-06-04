cf8f2dd23625b0fe7cdeae0db0267eabd62bc6f1
jqshi <jqshi@jqshis-MacBook-Air.local>
2025-06-05 06:06:20 +0800

    first init

---
+++ services/service-product/src/main/java/org/ben/service/product/ProductMainApplication.java
@@ -0,0 +1,12 @@
+package org.ben.service.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMainApplication.class, args);
+    }
+}
