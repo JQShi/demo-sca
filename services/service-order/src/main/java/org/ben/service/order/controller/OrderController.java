27cf4e8b57b35873984d87d8cfce395578ae9a3f
jqshi <jqshi@jqshis-MacBook-Air.local>
2025-06-05 22:33:16 +0800

    nacos config with properties

--- services/service-order/src/main/java/org/ben/service/order/controller/OrderController.java
+++ services/service-order/src/main/java/org/ben/service/order/controller/OrderController.java
@@ -1,22 +1,27 @@
 package org.ben.service.order.controller;
 
 import org.ben.model.order.Order;
+import org.ben.service.order.properties.OrderProperties;
 import org.ben.service.order.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
+//@RefreshScope
 @RestController
 public class OrderController {
 
-    @Value("${order.timeout}")
-    private String orderTimeout;
+//    @Value("${order.timeout}")
+//    private String orderTimeout;
+//
+//    @Value("${order.auto-confirm}")
+//    private String orderAutoConfirm;
 
-    @Value("${order.auto-confirm}")
-    private String orderAutoConfirm;
+    @Autowired
+    private OrderProperties orderProperties;
 
     private final OrderService orderService;
 
@@ -26,7 +31,7 @@ public class OrderController {
 
     @GetMapping("/order/config")
     public String getConfig() {
-        return this.orderTimeout + " : " + this.orderAutoConfirm;
+        return this.orderProperties.getTimeout() + " : " + this.orderProperties.getAutoConfirm();
     }
 
     @GetMapping("/order/create")
---
+++ services/service-order/src/main/java/org/ben/service/order/properties/OrderProperties.java
@@ -0,0 +1,16 @@
+package org.ben.service.order.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(value = "order")
+@Component
+@Data
+public class OrderProperties {
+
+    private String timeout;
+
+    private String autoConfirm;
+
+}
