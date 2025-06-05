9e9f6a7907e09cd2b54c0c42a1b3227cae48bb1c
jqshi <jqshi@jqshis-MacBook-Air.local>
2025-06-05 22:42:11 +0800

    test nacos config manager

--- services/service-order/src/main/java/org/ben/service/order/config/ServiceConfig.java
+++ services/service-order/src/main/java/org/ben/service/order/config/ServiceConfig.java
@@ -1,11 +1,20 @@
 package org.ben.service.order.config;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 @Configuration
+@Slf4j
 public class ServiceConfig
 {
     @LoadBalanced
@@ -13,4 +22,23 @@ public class ServiceConfig
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    public ApplicationRunner applicationRunner(NacosConfigManager nacosConfigManager) {
+        return args -> {
+            ConfigService configService = nacosConfigManager.getConfigService();
+            configService.addListener("service-order.properties", "DEFAULT_GROUP", new Listener() {
+                @Override
+                public Executor getExecutor() {
+                    return Executors.newSingleThreadExecutor();
+                }
+
+                @Override
+                public void receiveConfigInfo(String s) {
+                    log.info("config changed: {}", s);
+                }
+            });
+        };
+    }
+
 }
